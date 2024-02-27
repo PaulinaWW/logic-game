de54bf5ef060ce87961e26da50a16d3e441727de
PaulinaWW <159482754+PaulinaWW@users.noreply.github.com>
2024-02-27 17:43:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
 var scores, roundScore, activePlayer, playing;
 init();
 
@@ -15,18 +25,20 @@ document.querySelector('.btn--roll').addEventListener('click', function(){
     
     if(playing){
 
-    //1.Random Number
+    //creating random vaue for dice
     var dice = Math.floor(Math.random()*6)+1;
 
-    //2. Display the results
+    //displaying dice result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src= 'dice-' + dice + '.png';
     //document.querySelector('.dice').style.display = 'block';
     
 
-    //3. Update the round score if the rolled number was not a 1
-    if(dice!== 1){
+    // Update round score if rolled two 6 in a row
+    if ((dice1 === 6 && diceP1 === 6) || (dice2 === 6 && diceP2 === 6)) {
+      scores[activePlayer] = 0;
+
         //Add Score
         roundScore+=dice;
         document.querySelector('#current--' + activePlayer).textContent = roundScore;
